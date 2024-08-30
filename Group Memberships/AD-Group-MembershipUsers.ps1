fd5601c482fc6a532c880d2dc02ee45f66c6c1ce
Nate <natehess@protonmail.com>
2024-08-30 10:43:47 -0400

    Update AD-Group-MembershipUsers.ps1
    Added updates and error handling from Ad-GroupMembershipAl.ps1

--- Group Memberships/AD-Group-MembershipUsers.ps1
+++ Group Memberships/AD-Group-MembershipUsers.ps1
@@ -7,21 +7,23 @@ AUTHOR: nateahess
 DESCRIPTION: Script to list enabled users in a specified group or groups and export them to a CSV file. 
              Note - This script will not show nested groups. To view all MemberTypes see AD-GroupMembershipAll.ps1 
 
-
-TO USE: Add or change groups in the $groupNames variable that you wish yo get members for. 
-
 VERSION NOTES 
 
 > 1.0 | Initial Script creation and testing 
+> 1.1 | Copied updates from AD-GroupMembershipAll.ps1
 
 #> 
 
 #Check for ActiveDirectory Module 
 Write-Host "Loading Active Directory Module." 
 $admodule = Get-Module -ListAvailable | Where-Object {$_.Name -eq "ActiveDirectory"}
+
 if ($admodule -eq $null) {
+
     try {
+
         Install-Module -Name ActiveDirectory
+
     } catch {
 
         $errmsg = $_.ErrorMessage
@@ -33,42 +35,95 @@ if ($admodule -eq $null) {
 
 }
 
+
 Import-Module ActiveDirectory 
 
 Clear-Host 
 
+#Change execution policy 
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope LocalMachine 
+
+#Get current date for the filename 
+$date = (Get-Date).ToString("yyyMMdd")
+
 #Set group name(s) that you want to retrieve members for 
-$groupNames = @("Domain Admins")
+Write-Host "Please enter the group or groups you would like to retreive members for. (If more than one group, separate by comma)." 
+$userInput = Read-Host "> "
+$groupNames = $userInput -split ","
+
+#Get iput and set variable for file name 
+$filename = Read-Host "Please enter a name for the report (date is added automatically): "
 
 #Initialize an array to hold results 
 $data = @()
 
-#Loop through groups and get members 
+Write-Host " " 
+Write-Host "Looping through the group(s) $groupNames to retreive member information" 
+Write-Host "............................"
+
+#Loop through groups and get user members 
 foreach ($groupName in $groupNames) {
-    #Get the group members, filtering only users users 
-    $groupMembers = Get-ADGroupMember -Identity $groupName -Recursive | Where-Object {$_.objectClass -eq 'user'}
 
-    foreach ($member in $groupMembers) 
-        #Get detailed information about each member 
-        $user = Get-ADUser -Identity $member.SamAccountName -Properties Enabled 
+    try { 
 
-        #Filter only enabled accounts
-        if ($user.Enabled) {
-            #Add additional properties for exporting 
-            $user | Add-Member -Force -MemberType NoteProperty -Name GroupName -Value $groupnName 
-            $user | Add-Member -Force -MemberType NoteProperty -Name MemberType -Value "User" 
+        Write-Host ".....Retrieving data from $groupName" 
+        $groupMembers = Get-ADGroupMember -Identity $groupName 
 
-            #Add the user to the results table 
-            $data += $user 
-        }
+    } catch { 
+
+        Write-Host ".....Retreiving data from $groupName"
+        $groupMembers = Get-ADGroupMember -Identity $groupName 
+
+    }
+
+    $groupMembers = Get-ADGroupMember -Identity $groupNames
+
+    #Loop through groups and get user members 
+    foreach ($member in $groupMembers) { 
+
+        if ($member.objectClass -eq 'user') {
+
+                #Get information on each user 
+                $user = Get-ADUser -Identity $member.SamAccountName -Properties Name, SamAccountName, Enabled, Title, Department, Manager, Description
+
+                #Filter only enabled accounts 
+                if ($user.Enabled) { 
+
+                    #Create a custom object with additional properties 
+                    $userObject = [PSCustomObject]@{
+                        Name           = $user.name 
+                        SamAccountName = $user.SamAccountName
+                        GroupName      = $Groupname 
+                        MemberType     = "User"
+                        Enabled        = $user.Enabled 
+                        Title          = $user.title
+                        Department     = $useer.department 
+                        Manager        = $user.manager 
+                        Description    = $user.description 
+                    }
+
+                    #Add the user to the results table 
+                    $data += $userObject 
+                } 
+
+        }  else { 
+
+            Write-Host "$member is not of the object class 'user'" 
+            Write-Host "Skipping to the next user" 
+
+       }
     }
 }
+ 
 
+#Select desired properites and export to CSV 
+$userTable = $data | Select-Object Name, SamAccountName, GroupName, MemberType, Enabled, Title, Department, Manager, Description 
+$userTable | Export-Csv -Path "$PSScriptRoot\$filename-$date.csv" -NoTypeInformation 
 
-# Select desired properties and export to CSV 
-$userTable = $data | Select-Object Name, SamAccountName, GroupName, MemberType, Enabled 
-$userTable | Export-Csv -Path "$PSScriptRoot\..\GroupMemberships-Users.csv" -NoTypeInformation 
+Write-Host "Complete" 
+Write-Host "Report can be found at $PSScriptRoot\$filename-$date.csv" 
 
+pause 
 
 
 
