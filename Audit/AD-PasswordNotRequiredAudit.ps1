495a74ef933f0d923b7b51bbc34f57bdbe532ac1
Nate <natehess@protonmail.com>
2024-09-27 11:28:22 -0400

    Added AD-PasswordNotRequiredAudit.ps1
    Added new script for finding users that do not require a password to log in. Re-organized into new directories.

---
+++ Audit/AD-PasswordNotRequiredAudit.ps1
@@ -0,0 +1,60 @@
+<#
+
+TITLE: AD-PasswordNotRequiredCheck.ps1
+VERSION: 1.0
+AUTHOR: nateahess
+DATE: 9.4.2024
+DESCRIPTION: Script to locate accounts that do not require a password 
+
+
+VERSION NOTES: 
+
+1.0 | Initial script creation and testing. 
+
+#> 
+
+#Check for ActiveDirectory Module 
+Write-Host "Loading Active Directory Module." 
+$admodule = Get-Module -ListAvailable | Where-Object {$_.Name -eq "ActiveDirectory"}
+
+if ($admodule -eq $null) {
+
+    try {
+
+        Install-Module -Name ActiveDirectory
+
+    } catch {
+
+        $errmsg = $_.ErrorMessage
+        Write-Error "ActiveDirectory module is required for this script."
+        Write-Error "Please run PowerShell as Administrator and execute: Install-Module -Name ActiveDirectory then try again."
+        Write-Error $errmsg 
+        return 
+    }
+
+}
+
+
+Import-Module ActiveDirectory 
+
+Clear-Host 
+
+#Get current date for the filename 
+$date = (Get-Date).ToString("yyyMMdd")
+
+#Get filename 
+Write-Host "Please enter a filename for the export" 
+$filename = Read-Host "> "
+
+Write-Host "....Looking for accounts where PasswordNotRequired is set to True"
+
+#Get list of accounts from AD 
+Get-ADUser -Filter {PasswordNotRequired -eq $true} -Properties PasswordNotRequired | Select-Object Name, SamAccountName, Enabled, PasswordExpired, PasswordLastSet, Title, Department, Manager, Description | Select-Object
+
+Export-Csv -Path "$PSScriptRoot\$filename-$date.csv" -NoTypeInformation 
+
+Write-Host "....Complete"
+Write-Host "....CSV can be found at $PSScriptRoot\$filename-$date.csv" 
+
+pause 
+
--- README.md
+++ README.md
@@ -9,17 +9,21 @@
 
 ### Scripts in this toolkit (Will be updated as new scripts are added)
 
-* ### Forensics
 
-   - **AD-PasswordChange-Initiated-Audit** - Checks the last password change attempts for a user and displays the admin that initiated the change. 
+* ### Audit 
 
-* ### Stale Accounts
+    - **AD-PasswordNotRequiredAudit** - Finds user accounts that do not require a password to sign in 
 
     - **AD-StaleAccountsAudit** - Locates stale accounts in AD by looking for PasswordLastChanged and LastLogonTimestamp
 
     - **AD-StaleAccountsAudit2** - Locates stale accounts in AD by using the Search-ADUser -AccountInactive command 
 
     - **AD-ExpiredPasswordsAudit** - Provides a list of users with expired passwords 
+
+
+* ### Forensics
+
+   - **AD-PasswordChange-Initiated-Audit** - Checks the last password change attempts for a user and displays the admin that initiated the change. 
 
 
 * ### Group Memberships
@@ -40,7 +44,7 @@
 * ### Entra 
 
     - **ENTRA-GetUserLicenses** - Provides a list of users and their assigned licenses from Entra 
-    
+
     - **Entra-GetUserProxyAddresses** - Shows all proxy addresses for a specific user 
 
 
